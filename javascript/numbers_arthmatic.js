b6e523bc29a4810bce6dcb6a8857225464dcf657
itsmytime2518 <itsmytime169@gmail.com>
2024-10-04 15:24:02 +0000

    maths

--- javascript/numbers_arthmatic.js
+++ javascript/numbers_arthmatic.js
@@ -2,3 +2,10 @@
 const balance = new Number(100)
 console.log(balance);
 
+
+// ******************maths*************
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);

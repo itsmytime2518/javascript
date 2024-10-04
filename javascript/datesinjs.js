5f8339cec12f2fa0394aa322aa42aa46d56c94f1
itsmytime2518 <itsmytime169@gmail.com>
2024-10-04 15:57:13 +0000

    dates in js

--- javascript/datesinjs.js
+++ javascript/datesinjs.js
@@ -1,2 +1,14 @@
-let date = new Date()
+// let myDate = new Date()
+
+// console.log(myDate.getDate());
+// console.log(myDate.getDay());
+// console.log(myDate.getFullYear(),myDate.getDate(),myDate.getDay());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023,05,25)
+// let myCreatedDate = new Date(2023,5,25)
+// console.log(myCreatedDate.toDateString());
+// console.log(typeof myCreatedDate);
+
+
 

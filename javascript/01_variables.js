f65c5d193fbb10500d087cb2454ec02385c8036a
itsmytime2518 <itsmytime169@gmail.com>
2024-10-04 14:08:11 +0000

    string

--- javascript/01_variables.js
+++ javascript/01_variables.js
@@ -1,10 +1,50 @@
-const accountId ="254158";
-let accountEmail = "jjffkf.gmail.com";
-var accountPassword= 145214;
-accountCity = "ludhiana"
+// const accountId ="254158";
+// let accountEmail = "jjffkf.gmail.com";
+// var accountPassword= 145214;
+// accountCity = "ludhiana"
 
 // accountId = 2
 
-console.log(accountId);
+// console.log(accountPassword);
+// alert(3+6) we are using node  js not browser
 
+// console.log(accountEmail)
+// console.log( accountId);
+// console.log(accountPassword);
+// console.log(accountCity);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**2);
+
+let str1="hello "
+let str2="Megha"
+
+let str3 =str1 + str2
+
+console.log(str3);
+
+console.log(2<5);
+
+console.log(5>8);
+
+console.log(2==="2");
+
+console.log(5>=4);
+
+let myAge= {
+    name:"Megha Bajaj",
+    age:28,
+}
+
+console.log(myAge.name);
+
+const myFunction = function(){
+    console.log("hello world");
+}
+
+console.log(typeof myFunction);
 
---
+++ javascript/string.js
@@ -0,0 +1,8 @@
+let name = 'Megha'
+let repoCount = 28
+
+// console.log(name + repoCount+ " value"); out datted syntex
+
+console.log(`hello my name is ${name} and my age is ${repoCount}`);
+
+const gameName = new String (`Megha`)

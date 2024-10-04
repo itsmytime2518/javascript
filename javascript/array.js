0d5a3ee890c6d9139a74b6c5edd0cda52fced8d9
itsmytime2518 <itsmytime169@gmail.com>
2024-10-04 19:39:08 +0000

    array

---
+++ javascript/array.js
@@ -0,0 +1,40 @@
+
+
+
+// console.log(myArr);
+// console.log(myArr.length);
+// console.log(myArr.push(100));
+// console.log(myArr.unshift(9));
+// console.log(myArr.shift());
+
+
+
+// console.log(myArr.includes(8));
+// console.log(myArr.indexOf(7));
+
+// console.log(typeof myArr);
+// console.log(typeof myArray);
+
+
+// slice,splice
+
+let myArr = ["thor", "tony stark", "Iron Man", "Hulk", 551, 'flash']
+
+console.log("A",myArr);
+
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B",myArr);
+
+
+const myn2 = myArr.splice(1,2)
+console.log("c" , myn2);
+
+
+
+
+
+
+
+

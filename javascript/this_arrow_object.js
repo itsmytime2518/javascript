298dacf1731442b08b93adcc159f7b9a2746ef3f
itsmytime2518 <itsmytime169@gmail.com>
2024-10-07 07:45:04 +0000

    loops and string

--- javascript/this_arrow_object.js
+++ javascript/this_arrow_object.js
@@ -2,4 +2,12 @@
 
 
 // **********************arrow****************************
+const heading= document.getElementById("heading");
+
+const button= document.getElementById("changeTextButton");
+
+
+button.addEventListener("click",function(){
+    heading.innerText = "i love you Megha!";
+});
 

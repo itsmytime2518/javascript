a7812f98b5880c28e2c8d312124c76a49f5cc364
itsmytime2518 <itsmytime169@gmail.com>
2024-10-07 10:18:32 +0000

    loops string

---
+++ javascript/loops_string.js
@@ -0,0 +1,74 @@
+// console.log("apna college");
+// console.log("apna college");
+// console.log("apna college");
+// console.log("apna college");
+// console.log("apna college");
+// console.log("apna college");
+// console.log("apna college");
+// console.log("apna college");
+// console.log("apna college");
+// console.log("apna college");
+// console.log("apna college");
+// console.log("apna college");
+
+
+function myloop() {
+    for (let index = 1; index <= 5; index++) {
+        const element = index;
+        console.log(element);
+        
+    }
+
+    return "loop complete";
+
+}
+
+
+
+
+const result1 = myloop()
+
+console.log(result1);
+
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+
+    
+// }
+
+
+// const  a = 'this is my first step for javascript'
+
+
+
+// console.log(a);
+
+
+let geet = 0
+
+const n = 100
+
+for (let i = 1; i <= n; i++) {
+    geet = geet + i;
+    console.log(i);
+    
+    
+}
+
+console.log( "loop result is = ",geet);
+
+
+
+// console.log(Slaudin);
+
+
+
+
+
+
+
+
+
+
+

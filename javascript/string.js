14c6b4e4ba5fd7771f73bfe5dd794b8e8b031d95
itsmytime2518 <itsmytime169@gmail.com>
2024-10-08 10:05:42 +0000

    loops and string

--- javascript/string.js
+++ javascript/string.js
@@ -1,14 +1,16 @@
-let name = 'Megha'
-let repoCount = 28
+// let name = 'Megha'
+// let repoCount = 28
 
-// console.log(name + repoCount+ " value"); out datted syntex
+// // console.log(name + repoCount+ " value"); out datted syntex
 
-console.log(`hello my name is ${name} and my age is ${repoCount}`);
+// console.log(`hello my name is ${name} and my age is ${repoCount}`);
 
-const gameName = new String (`   Megha   `)
+// const gameName = new String (`   Megha   `)
 
-const gameName01 = gameName
+// const gameName01 = gameName
 
-// console.log(gameName01.trim());
+// // console.log(gameName01.trim());
 
-console.log(gameName01);
+// console.log(gameName01);
+
+

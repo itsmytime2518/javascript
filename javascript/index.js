0e7a3a279cecc8a49e5388527da86583aba200ed
itsmytime2518 <itsmytime169@gmail.com>
2024-10-09 20:43:04 +0000

    practice

--- javascript/index.js
+++ javascript/index.js
@@ -2,3 +2,8 @@ let heading = document.getElementById("heading")
 
 let change =  heading.innerHTML = "hello this is my first statment for inner.HTML"
 
+
+let div2 = document.querySelector(".div2")
+let div3 = div2.innerHTML = "hello Slaudin Ali"
+
+
